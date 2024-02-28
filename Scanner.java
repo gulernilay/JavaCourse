c546dc09e55e49157744dd361587da7fefa0e248
Nilay Güler <gulernilay088@gmail.com>
2024-02-28 18:29:01 +0300

    Input / Output part is updated

--- Scanner.java
+++ Scanner.java
@@ -94,19 +94,71 @@ public class InputOutput{
         char gender = sc.next().charAt(0);
       
     }
-
-
-
-
-
-
-
+}
 }
 
+/*
+// Working program using Scanner 
+import java.io.BufferedReader; 
+import java.io.InputStreamReader; 
+import java.util.Scanner; 
+public class Main { 
+    public static void main(String[] args) 
+    { 
+        Scanner s = new Scanner(System.in); 
+        int n = s.nextInt(); 
+        int k = s.nextInt(); 
+        int count = 0; 
+        while (n-- > 0) { 
+            int x = s.nextInt(); 
+            if (x % k == 0) 
+                count++; 
+        } 
+        System.out.println(count); 
+    } 
+}
+
+// Working program using BufferedReader 
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+import java.util.StringTokenizer; 
   
-
-
-
-
-
+public class Main { 
+    public static void main(String[] args) 
+        throws IOException 
+    { 
+  
+        BufferedReader br = new BufferedReader( 
+            new InputStreamReader(System.in)); 
+  
+        StringTokenizer st 
+            = new StringTokenizer(br.readLine()); 
+        int n = Integer.parseInt(st.nextToken()); 
+        int k = Integer.parseInt(st.nextToken()); 
+        int count = 0; 
+        while (n-- > 0) { 
+            int x = Integer.parseInt(br.readLine()); 
+            if (x % k == 0) 
+                count++; 
+        } 
+        System.out.println(count); 
+    } 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
